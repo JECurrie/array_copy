805c3b0ce40a2a247955c7502e8186ea70066d7c
Joan Currie <jecurrie8@gmail.com>
2018-06-02 03:00:34 +0000

    Copy the values less than 4 in the array stored in the source variable into the array in the destination variable.

---
+++ array-ruby.rb
@@ -0,0 +1,18 @@
+# Create and display an array.
+names = Array["Conrad", "James", "Joyce"]
+puts names #, ""
+puts
+
+# Pop the last element.
+names.pop()
+##puts names #, ""
+puts
+
+# Pop again.
+names.pop()
+##puts names #, ""
+
+puts "hi"
+# Pop again and see if the array is empty.
+names.pop()
+puts names.empty?
